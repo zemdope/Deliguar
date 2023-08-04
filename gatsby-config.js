82c67ee09f904c153ee3da95f2697eaba2b401d8
przemekGrupaImprove <przemyslaw.michalowski@grupa-improve.pl>
2023-08-04 11:16:27 +0200

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,20 +2,27 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  siteMetadata: {
-    title: `Deliguar`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: [{
-    resolve: 'gatsby-source-contentful',
-    options: {
-      "accessToken": "374CyfgtGTC5EPs69r9GyvT-mbbFQu6P5NGgwF0uufw",
-      "spaceId": ""
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-styled-components", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }]
+	siteMetadata: {
+		title: `Deliguar`,
+		siteUrl: `https://www.yourdomain.tld`
+	},
+	plugins: [
+		{
+			resolve: 'gatsby-source-contentful',
+			options: {
+				accessToken: 'e-zmhxgJB2pTqsgNPQSzdtJ3bRXIo6EQuo34qJZ_xio',
+				spaceId: 'w5u0yujeh638'
+			}
+		},
+		'gatsby-plugin-image',
+		'gatsby-plugin-sharp',
+		'gatsby-transformer-sharp',
+		'gatsby-plugin-styled-components',
+		{
+			resolve: 'gatsby-plugin-manifest',
+			options: {
+				icon: 'src/images/icon.png'
+			}
+		}
+	]
 };
