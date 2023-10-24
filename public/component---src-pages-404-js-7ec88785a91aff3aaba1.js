7c3079b31037ad3ccba2cee63ffd1a3b6e3f631f
Przemysław Michałowski <przemek@MacBook-Air-Przemysaw.local>
2023-10-24 14:44:47 +0200

    fix

---
+++ public/component---src-pages-404-js-7ec88785a91aff3aaba1.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkdeliguar=self.webpackChunkdeliguar||[]).push([[883],{429:function(e,t,n){n.r(t),n.d(t,{Head:function(){return s}});var a=n(7294);n(4160);const l={color:"#232129",padding:"96px",fontFamily:"-apple-system, Roboto, sans-serif, serif"};t.default=()=>a.createElement("main",{style:l});const s=()=>a.createElement("title",null,"Not found")}}]);
+//# sourceMappingURL=component---src-pages-404-js-7ec88785a91aff3aaba1.js.map
